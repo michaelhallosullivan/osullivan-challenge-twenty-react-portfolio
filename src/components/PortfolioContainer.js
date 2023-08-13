399a06e0f016f122e8c79fdd51a8304a9718f37d
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-08-12 21:42:10 -0700

    added skeleton css framework, public directory, and basic formatting

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import NavTabs from './NavTabs';
 import Home from './pages/Home';
 import About from './pages/About';
-import Blog from './pages/Blog';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
@@ -16,8 +17,11 @@ export default function PortfolioContainer() {
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
     return <Contact />;
   };
